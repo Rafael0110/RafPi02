e3a1089ea308e050b3cb838e61f365e3fdef0ac6
Rafael0110 <s1331068BV@s.chibakoudai.jp>
2016-11-26 01:48:36 +0000

    点滅回数変更

--- LED.py
+++ LED.py
@@ -2,15 +2,15 @@
 import RPi.GPIO as GPIO
 import time
 
-COUNT = 3
+COUNT = 10
 PIN = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN,GPIO.OUT)
 
 for _ in xrange(COUNT):
     GPIO.output(PIN,True)
-    time.sleep(1.0)
+    time.sleep(0.2)
     GPIO.output(PIN,False)
-    time.sleep(1.0)
+    time.sleep(0.2)
 
 GPIO.cleanup()
