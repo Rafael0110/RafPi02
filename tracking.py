4f1d430a376d0ec62546fb78fc9cdea12fe231e6
rafael sasaki <s1331068bv@s.chibakoudai.jp>
2017-02-19 17:21:36 +0900

    赤色追跡

---
+++ tracking.py
@@ -0,0 +1,69 @@
+#Red Object Tracking for python3
+import io
+import picamera
+import pygame
+from pygame.locals import *
+
+debug = 1
+WIDTH = 480
+HEIGHT = 360
+margin = 50*debug
+step = 2
+dimw = int(WIDTH/step)
+dimwh= int(HEIGHT/step)
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((WIDTH+margin, HEIGHT+margin))
+pygame.display.set_mode('Red Object Tracking')
+camera = picamera.PiCamera()
+camera.resolution = (WIDTH,HEIGHT)
+stream = io.BytesIO()
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+endflag = 0
+
+while endflag == 0:
+	for event in pygame.event.get():
+		if event.type == QUIT: endflag = 1
+
+	DISPLAYSURF.fill(BLACK)
+	stream.seek(0)
+	camera.capture(stream,'rgb')
+	sumx = [0] * dimw
+	sumy = [0] * dimwh
+	for y in range(0,HEIGHT,step):
+		for x in range(0,WIDTH,step):
+			stream.seek((x+(y*WIDTH))*3)
+			r = ord(stream.read(1))
+			g = ord(stream.read(1))
+			b = ord(stream.read(1))
+			rv = int(r-((g+b)/2))
+			if rv < 0 : rv = 0
+			sumx[int(x/step)] += rv
+			sumy[int(y/step)] += rv
+			pygame.draw.rect(DISPLAYSURF ,(r,g,b) ,(x,y,step,step))
+
+	tx = step * (sumx.index(max(sumx)))
+	ty = step * (sumy.index(max(sumy)))
+	if debug:
+		x = WIDTH
+		for y in range(0,HEIGHT,step):
+			ave = sumy[int(y/step)]/dimw
+			pygame.draw.line(DISPLAYSURF ,RED,(x,y),(x,y+ave),1)
+
+		y = HEIGHT
+		for x in range(0,WIDTH,step):
+			ave = sumx[int(x/step)]/dimh
+			pygame.draw.line(DISPLAYSURF ,RED,(x,y),(x,y+ave),1)
+
+		pygame.draw.line(DISPLAYSURF ,WHITE,(tx,0),(tx,HEIGHT),1)
+		pygame.draw.line(DISPLAYSURF ,WHITE,(0,ty),(WIDTH,ty),1)
+	else:
+		pygame.draw.circle(DISPLAYSURF ,BLACK,(tx-30,ty-30),8)
+		pygame.draw.circle(DISPLAYSURF ,BLACK,(tx+30,ty-30),8)
+		pygame.draw.arc(DISPLAYSURF ,BLACK,(tx-30,ty-30,60,60),3.14,6.28,4)
+
+	pygame.display.update()
+pygame.quit()
+camera.close()
